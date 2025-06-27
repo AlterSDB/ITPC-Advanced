0a74670842c11ca8f10b950028462cfaa073e96a
Anders <morgenstern089@gmail.com>
2025-06-27 21:43:58 +0500

    Refactor DataFile class
    Added DataParser class + Updated dependencies

---
+++ src/main/java/org/itpc_advanced/App/model/DataFileNew.java
@@ -0,0 +1,38 @@
+package org.itpc_advanced.App;
+
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.Date;
+import java.util.List;
+
+public class DataFileN {
+	private String tcType = null;
+	private LocalDateTime timeStamp = null;
+	private Double timeStep = null;
+	private List<Double> values = null;
+	
+	public DataFileN(String tcType, LocalDateTime timeStamp, Double timeStep, List<Double> values) {
+		this.tcType = tcType;
+		this.timeStamp = timeStamp;
+		this.timeStep = timeStep;
+		this.values = values;
+	}
+	
+	public String getTcType() {
+		return tcType;
+	}
+
+	public LocalDateTime getTimeStamp() {
+		return timeStamp;
+	}
+
+	public Double getTimeStep() {
+		return timeStep;
+	}
+
+	public List<Double> getValues() {
+		return values;
+	}
+		
+}
+	
